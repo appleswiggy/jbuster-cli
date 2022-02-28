256e012e7e916ff321c4605e32158cac80e87788
chirag-madaan <madchirag546@gmail.com>
2022-02-28 21:47:09 +0530

    add banner

---
+++ src/main/java/com/github/appleswiggy/Banner.java
@@ -0,0 +1,25 @@
+package com.github.appleswiggy;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Banner {
+    public static void printBanner(String url, String wordlist, String userAgent) {
+        LocalDateTime dateObj = LocalDateTime.now();
+        DateTimeFormatter formatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+        System.out.println(Global.partitioner);
+        System.out.println(Global.tool + " " + Global.version);
+        System.out.println(Global.credits);
+        System.out.println(Global.partitioner);
+
+        System.out.println("[+] URL:\t" + url);
+        System.out.println("[+] Wordlist:\t" + wordlist);
+        System.out.println("[+] User Agent:\t" + userAgent);
+
+        System.out.println(Global.partitioner);
+        System.out.println(dateObj.format(formatObj) + " Starting Jbuster");
+        System.out.println(Global.partitioner);
+
+    }
+}
--- src/main/java/com/github/appleswiggy/JbusterCliCommand.java
+++ src/main/java/com/github/appleswiggy/JbusterCliCommand.java
@@ -8,21 +8,23 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-@Command(name = "jbuster-cli", description = "Web directory/file busting CLI tool in Java.", mixinStandardHelpOptions = true)
+@Command(name = "jbuster-cli", description = "Web directory/file busting CLI tool in Java.", mixinStandardHelpOptions = true, version = "Jbuster-cli v1.0")
 public class JbusterCliCommand implements Runnable {
 
     @Option(names = { "-u", "--url" }, description = "URL of the target.", required = true)
     String url;
 
     @Option(names = { "-w", "--wordlist" }, description = "Wordlist to be used.", required = true)
-    String wordllist;
+    String wordlist;
+
+    @Option(names = { "-U", "--user-agent" }, description = "User-Agent HTTP header", defaultValue = "Jbuster-cli/1.0")
+    String userAgent = Global.userAgent;
 
     public static void main(String[] args) throws Exception {
         PicocliRunner.run(JbusterCliCommand.class, args);
     }
 
     public void run() {
-        System.out.println("URL given: " + url);
-        System.out.println("Wordlist: " + wordllist);
+        Banner.printBanner(url, wordlist, userAgent);
     }
 }
